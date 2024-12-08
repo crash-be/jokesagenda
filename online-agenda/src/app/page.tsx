71c77f1cf19b4b80eac8e681d668392fd7bb6791
unknown <martijn.vanlede@gmail.com>
2024-12-08 16:09:10 +0100

    Initial commit

---
+++ online-agenda/src/app/page.tsx
@@ -0,0 +1,9 @@
+import DagAgenda from '../components/DagAgenda';
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <DagAgenda />
+    </main>
+  );
+}
