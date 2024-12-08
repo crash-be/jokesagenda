5133505e20b25e5d5c031ba0f39a66864419ac05
crash-be <martijn.vanlede@gmail.com>
2024-12-08 16:39:00 +0100

    Delete online-agenda directory

--- online-agenda/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DagAgenda from '../components/DagAgenda';
-
-export default function Home() {
-  return (
-    <main className="container mx-auto p-4">
-      <DagAgenda />
-    </main>
-  );
-}
